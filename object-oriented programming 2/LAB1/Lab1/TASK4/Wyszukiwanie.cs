4614d4f9ce873458577f68ecf3a900f9b4556acf
Dawid Olko <dawid_olko@outlook.com>
2023-12-03 10:37:35 +0100

    Add files via upload

---
+++ object-oriented programming 2/LAB1/Lab1/TASK4/Wyszukiwanie.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1.TASK4
+{
+
+    public class Program4
+    {
+        public static void Wyszukiwanie()
+        {
+            double[] liczby = new double[10];
+            double suma = 0, iloczyn = 1, srednia;
+            double min = double.MaxValue, max = double.MinValue;
+
+            Console.WriteLine("Wprowadź 10 liczb:");
+            for (int i = 0; i < liczby.Length; i++)
+            {
+                Console.Write($"Liczba {i + 1}: ");
+                liczby[i] = Convert.ToDouble(Console.ReadLine());
+
+                suma += liczby[i];
+                iloczyn *= liczby[i];
+
+                if (liczby[i] < min)
+                    min = liczby[i];
+                if (liczby[i] > max)
+                    max = liczby[i];
+            }
+
+            srednia = suma / liczby.Length;
+
+            Console.WriteLine($"\nSuma elementów: {suma}");
+            Console.WriteLine($"Iloczyn elementów: {iloczyn}");
+            Console.WriteLine($"Średnia wartość: {srednia}");
+            Console.WriteLine($"Wartość minimalna: {min}");
+            Console.WriteLine($"Wartość maksymalna: {max}");
+        }
+    }
+
+}
---
+++ object-oriented programming 2/LAB2/Lab2/TASK1/Licz.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    public class Licz
+    {
+        private double value; // Prywatne pole przechowujące wartość liczbową
+
+        // Konstruktor inicjujący pole 'value'
+        public Licz(double initialValue)
+        {
+            value = initialValue;
+        }
+
+        // Metoda dodająca wartość do pola 'value'
+        public void Dodaj(double amount)
+        {
+            value += amount;
+        }
+
+        // Metoda odejmująca wartość od pola 'value'
+        public void Odejmij(double amount)
+        {
+            value -= amount;
+        }
+
+        // Metoda wypisująca stan obiektu
+        public void WypiszStan()
+        {
+            Console.WriteLine("Aktualna wartość: " + value);
+        }
+    }
+
+}
