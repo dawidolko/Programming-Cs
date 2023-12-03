8bdadb6624318ec6768e0657f2afaa95844da6f3
Dawid Olko <dawid_olko@outlook.com>
2023-12-03 11:00:49 +0100

    Create repository Course 1

---
+++ Courses/Course1/TASK1/lekcja1/Program.cs
@@ -0,0 +1,5 @@
+﻿// 0. Kurs C# dla początkujących - Piszemy program "Hello World" - [https://www.youtube.com/watch?v=qVMX17uoHbk&list=PL6aekdNhY7DAeU-2LEGBDXls60Yy-Ubbu&index=2]
+
+Console.WriteLine("Podaj swoje imię");
+string imie = Console.ReadLine();
+Console.WriteLine("Witaj " + imie);
---
+++ Courses/Course1/TASK36/lekcja36/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace lekcja36
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Zwierze zwierze = new Zwierze();
+            Kot kot = new Kot();
+            Console.WriteLine("Zwierze:");
+            zwierze.DajGlos();
+            Console.WriteLine("Kot:");
+            kot.DajGlos();
+            kot.PijMleko();
+            //zwierze = new Kot();
+
+            Console.WriteLine("Czy kot jest kotem?");
+            if (kot is Kot)
+            {
+                Console.WriteLine("Powiodło się");
+            }
+            else
+            {
+                Console.WriteLine("Nie powiodło się");
+            }
+            Console.WriteLine("Czy zwierze jest kotem?");
+            if (zwierze is Kot)
+            {
+                Console.WriteLine("Powiodło się");
+            }
+            else
+            {
+                Console.WriteLine("Nie powiodło się");
+            }
+
+            //Kot kot3 = (Kot)zwierze;
+            Kot kot2 = zwierze as Kot;
+
+            if (!(kot2 == null))
+            {
+                Console.WriteLine("Powiodło się");
+            }else
+            {
+                Console.WriteLine("Nie powiodło się");
+            }
+            //kot2.PijMleko(); nie ma tej metody gdyż kot2 powstał na bazie zwierze
+
+        }
+    }
+}
